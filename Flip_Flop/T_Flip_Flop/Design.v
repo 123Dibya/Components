00cdd5be1afb7bf1f2350a126f9c8d04504f8800
Dibya Prakash Subudhi <156248743+123Dibya@users.noreply.github.com>
2024-12-22 12:53:43 +0530

    Update Design.v

--- Flip_Flop/T_Flip_Flop/Design.v
+++ Flip_Flop/T_Flip_Flop/Design.v
@@ -1,1 +1,19 @@
+`timescale 1ns / 1ps
+module flip (
+    input clk,
+    input reset,  
+    input t,
+    output reg q
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        q <= 0;  
+    else if (t) 
+        q <= ~q;  
+    else 
+        q <= q;  
+end
+
+endmodule
 
